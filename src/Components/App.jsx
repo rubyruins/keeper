8bd84d560ed879239265626d18005285262b395d
rubyruins <soumya.parekh@somaiya.edu>
2020-03-29 00:36:13 +0530

    added example notes with props

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+function createNote(note) {
+    return(<Note title = {note.title} content={note.content} />);
+}
 
 function App() {
-    return <div><Header /><Note /><Footer /></div>;
+    return <div><Header />{notes.map(createNote)}<Footer /></div>;
 };
 
 export default App;
